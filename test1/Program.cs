08562175b7bd0c6464293611f263480056fd8184
SenjuWhite <nikitaskraga6@gmail.com>
2023-03-14 18:28:26 +0200

    Initial commit

---
+++ test1/Program.cs
@@ -0,0 +1,44 @@
+﻿int height = int.Parse(Console.ReadLine());
+for (int i = 1; i <= height; i++)
+{
+	for (int j = 1; j <= i; j++)
+	{
+		Console.Write("#");
+	}
+	Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = height; i > 0; i--)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+for (int i = 1; i <= height; i++)
+{
+	for (int j = 1; j <=height-i; j++)
+	{
+		Console.Write(" ");
+	}
+	for (int k = 0; k<i; k++)
+	{
+		Console.Write("#");
+	}
+	Console.WriteLine();
+}
+Console.WriteLine( );
+for (int i = height; i > 0; i--)
+{
+    for (int j = 1; j <= height - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 0; k < i; k++)
+    {
+        Console.Write("#");
+    }
+    Console.WriteLine();
+}
+
